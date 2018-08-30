abb4b9619cf8ee8b6d3aa0577f9b367ee394ba28
Konrad Leszczyński <konrri@man.poznan.pl>
2018-08-30 09:54:05 +0200

    empty pod lib

---
+++ Example/Pods/Target Support Files/Pods-SymbioteIosUtils_Example/Pods-SymbioteIosUtils_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SymbioteIosUtils_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SymbioteIosUtils_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-SymbioteIosUtils_Tests/Pods-SymbioteIosUtils_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SymbioteIosUtils_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SymbioteIosUtils_Tests
+@end
